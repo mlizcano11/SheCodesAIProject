2224716d8fdb62e194436d2ccdc971716c2e6ba9
Melinda L. <lizcanomelinda@outlook.com>
2025-02-21 15:09:13 -0800

    integrated AI API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generatePoem(event) {
-    event.preventDefault();
-
+function displayPoem(response) {
     new Typewriter("#poem", {
-        strings: "The earth shakes just enough to remind us",
+        strings: response.data.answer,
         autoStart: true, 
         delay: 1, 
         cursor: "",
     });
+}
+
+function generatePoem(event) {
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "0da3f0051b47b44c6b1o0t3aafd76c05"
+    let context = "You are a intelligent Haiku poem expert. You are expected to generate a Haiku poem, of three lines and 17 syllables in HTML with <br> to separate each line. Please follow the user topic. Do not include a poem title.";
+    let prompt = `User instructions: Generate a Haiku poem about ${instructionsInput.value}`;
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating"> ⏰ Creating a Haiku about ${instructionsInput.value} </div>`;
+
+    axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
